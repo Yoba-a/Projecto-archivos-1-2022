5666a3e531e140e22466289f6fb9dc1a3800fffe
Yoba <3016834760101@ingenieria.usac.edu.gt>
2022-03-18 20:46:17 -0600

    proyecto de archivos

---
+++ p.sh
@@ -0,0 +1,6 @@
+Mkdisk -Size=6 -unit=m  -path=/home/scast/proyecto_archivos/disco2.disk
+fdisk -size =1 -unit=m  -path=/home/scast/proyecto_archivos/disco2.disk -name= particion1
+fdisk -size =1 -unit=m  -path=/home/scast/proyecto_archivos/disco2.disk -name= particion2
+fdisk -size =1 -unit=m  -path=/home/scast/proyecto_archivos/disco2.disk -name= particion3
+fdisk -size =1 -unit=m  -path=/home/scast/proyecto_archivos/disco2.disk -name= particion4
+rep = /home/scast/proyecto_archivos/disco2.disk
